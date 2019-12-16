9cd19b37ac16b3a31b7ea81a06426af30da66f0c
Stephen Bailey <stephen.bailey@audioanalytic.com>
2019-12-16 00:24:39 +0000

    [Day10] Hack at part2.
    Doesn't yet work - need to double check that relative vectors
    are actually being used, rather than initial points.

--- day10.py
+++ day10.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, atan, sin, cos
 
 vec1 = [".#..#",
         ".....",
@@ -27,38 +27,77 @@ def parse_map(width, height, vec_in):
 def find_distance_vector(a, b):
   return(a[0] - b[0], a[1] - b[1])
 
-def normalise_vectors(a):
+def normalise_vector(a):
   mag = sqrt(a[0]*a[0] + a[1]*a[1])
-  return (round(a[0]/mag, 7), round(a[1]/mag, 7))
+  return (round(a[0]/mag, 7), round(a[1]/mag, 7), mag)
 
-def find_visible_neighbours(asteroids):
+def get_visible_neighbours(point, array):
+  this = array.index(point)
+  other_ast = [find_distance_vector(b, point) for b in array]
+  other_ast.pop(this)
+
+  norm = {}
+  for b in other_ast:
+    vecx, vecy, mag = normalise_vector(b)
+    try:
+      if mag < norm[(vecx, vecy)]:
+        norm.update({(vecx, vecy):mag})
+    except KeyError:
+      norm.update({(vecx, vecy):mag})
+  return(norm)
+
+def find_base(asteroids):
   visible_neighbours = [0]*len(asteroids)
-  print("total: ", len(asteroids))
+
   for idx, a in enumerate(asteroids):
-    other_ast = [find_distance_vector(b, a) for b in asteroids]
-    other_ast.pop(idx)
-    # print(a)
-    norm = [normalise_vectors(b) for b in other_ast]
-    # print("init: ", norm)
-    # print("norm: ", list(set(norm)))
-    visible_neighbours[idx] = len(list(set(norm)))
-  # print(asteroids)
-  # print(visible_neighbours)
-  print(asteroids[visible_neighbours.index(max(visible_neighbours))])
-  print(max(visible_neighbours))
+    norm = get_visible_neighbours(a, asteroids)
+    visible_neighbours[idx] = len(norm)
+
+  base = asteroids[visible_neighbours.index(max(visible_neighbours))]
+  visible_neighbours = get_visible_neighbours(base, asteroids)
+  return (base, visible_neighbours)
 
 def test_1():
   asteroids = parse_map(5,5,vec1)
-  # print(asteroids)
-  find_visible_neighbours(asteroids)
+  b,v = find_base(asteroids)
+  print(b, len(v)) # expecting 3,4
 
 def part_1():
   w,h,data_in = load_input("day10")
   # print(w,h,data_in)
   asteroids = parse_map(w,h,data_in)
   # print(asteroids)
-  find_visible_neighbours(asteroids)
+  b,v = find_base(asteroids)
+  print(b, len(v))
+
+def part_2():
+  w,h,data_in = load_input("day10")
+  # print(w,h,data_in)
+  asteroids = parse_map(w,h,data_in)
+
+  base, targets = find_base(asteroids)
+  polar_form = []
+  count = 0
+  for k, mag in targets.items():
+    
+    print(count, "- ", (k[1],k[0]), "- ")
+    try:
+      print((atan(k[1]/k[0]), mag))
+      polar_form.append((atan(k[1]/k[0]), mag))
+    except ZeroDivisionError:
+      polar_form.append((0 , mag))
+      # print(count, "- ", (k[1],k[0]), "- ")
+    count += 1
+
+  print(base)
+  polar_form.sort()
+  for idx in range(198,202,1):
+    res = polar_form[idx]
+    print("[{}] = {}, {}".format(idx, 
+      round(res[1] * cos(res[0]), 3),
+      round(res[1] * sin(res[0]), 3)))
 
 if __name__ == "__main__":
   #test_1()
   part_1()
+  part_2()
