cc636cccb498e59439f7ad703e2ef327a7daee05
Stephen Bailey <stephen.bailey@audioanalytic.com>
2019-12-18 13:05:16 +0000

    [Day12] Solution for part 12.

--- day12.py
+++ day12.py
@@ -1,4 +1,5 @@
 import re
+
 input_re = r"<x=(-?\d*), y=(-?\d*), z=(-?\d*)>"
 
 class Moon(object):
@@ -41,7 +42,52 @@ def part_1():
       moon.update_velocity(other_moons)
     for moon in moons:
       moon.update_position()
-  print(sum(abs(m.energy) for m in moons))  
+  print(sum(abs(m.energy) for m in moons))
+  print("expecting 7471")
+
+def check_looped(moons, start_positions, axis):
+  for moon in moons:
+    if 0 != moon.velocity[axis]:
+      return False
+  for idx, moon in enumerate(moons):
+    if moon.position[axis] != start_positions[idx][axis]:
+        return False
+  return True
+
+def gcd(a,b):
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b"""
+    return a * b / gcd(a, b)
+
+def part_2():
+  moons = load_input("day12")
+  moon_start = [list(m.position) for m in moons]
+  loop_points = []
+  for axis in range(3):
+    moons = load_input("day12")
+    count = 0
+    while(True):
+      for moon in moons:
+        other_moons = [m.position for m in moons if m != moon]
+        moon.update_velocity(other_moons)
+      for moon in moons:
+        moon.update_position()
+      count += 1
+      if check_looped(moons, moon_start, axis):
+        print("[{}]Success Loop: {}".format(axis, count))
+        # for moon in moons:
+        #   print(moon.position, moon.velocity)
+        loop_points.append(count)
+        break
+  a = lcm(loop_points[0], loop_points[1])
+  b = lcm(a, loop_points[2])
+  print(b)
 
 if __name__ == "__main__":
   part_1()
+  part_2()
